56e81ce461c33a111fc322d33d4d9c0340651d18
emsoraffa <emil.rafael.solberg@gmail.com>
2025-10-10 16:12:05 +0200

    feat color.h

---
+++ src/color.h
@@ -0,0 +1,24 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+#include "vec3.h"
+
+#include <iostream>
+
+using color = vec3;
+
+void write_color(std::ostream &out, const color &pixel_color) {
+  double r = pixel_color.x();
+  double g = pixel_color.y();
+  double b = pixel_color.z();
+
+  // Translate the [0, 1] component values to byte range [0, 255]
+  int rbyte = int(255.999 * r);
+  int gbyte = int(255.999 * g);
+  int bbyte = int(255.999 * b);
+
+  // Write out the pixel color components
+  out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
+}
+
+#endif
